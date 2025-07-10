d02ff4951214ace8f92904ffc107bdb7762f4675
infant@weseeble.com <infant@weseeble.com>
2025-07-10 09:51:04 +0530

    Initial commit

---
+++ src/main/java/com/example/probe_navigator/ProbeNavigatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.probe_navigator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProbeNavigatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProbeNavigatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/probe_navigator/ProbeNavigatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.probe_navigator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProbeNavigatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
